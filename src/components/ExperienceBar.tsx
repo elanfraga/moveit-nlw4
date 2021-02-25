3f58d8db11e36b26706e60055609b9adf9cba36c
Elan Fraga <elanfraga@gmail.com>
2021-02-25 01:49:31 -0300

    challegebox, compleredchallges and context api

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export default function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+
+  const precentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
+
   return (
     <header className={styles.experienceBar} >
       <span>0 xp</span>
       <div>
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${precentToNextLevel}%` }} />
 
-        <span className={styles.currentExperience} style={{ left: '50%' }} >
-          300xp
+        <span className={styles.currentExperience} style={{ left: `${precentToNextLevel}%` }} >
+          {currentExperience} xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>{experienceToNextLevel} xp</span>
     </header>
   )
 };
